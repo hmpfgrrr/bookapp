e0200aa6a25c49f8b9e7de99ee18f368eb7bbae8
Christian Neuls <christian.neuls@brandad-systems.de>
2023-06-09 13:00:06 +0200

    changed name to bookapp

---
+++ src/main/java/com/example/bookapp/BookController.java
@@ -0,0 +1,51 @@
+package com.example.bookapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Book book = bookService.getBookById(id);
+        if (book != null) {
+            return ResponseEntity.ok(book);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book createdBook = bookService.createBook(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdBook);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        Book updatedBook = bookService.updateBook(id, book);
+        if (updatedBook != null) {
+            return ResponseEntity.ok(updatedBook);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/test/java/com/example/bookapp/BookServiceTest.java
@@ -0,0 +1,41 @@
+package com.example.bookapp;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.bookapp.Book;
+import com.example.bookapp.BookRepository;
+import com.example.bookapp.BookService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class BookServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Test
+    public void testGetAllBooks() {
+        Book book1 = new Book("Title 1", "Author 1", "1234567890");
+        Book book2 = new Book("Title 2", "Author 2", "0987654321");
+        List<Book> books = Arrays.asList(book1, book2);
+
+        when(bookRepository.findAll()).thenReturn(books);
+
+        List<Book> result = bookService.getAllBooks();
+
+        assertEquals(2, result.size());
+        assertEquals(book1, result.get(0));
+        assertEquals(book2, result.get(1));
+    }
+}
