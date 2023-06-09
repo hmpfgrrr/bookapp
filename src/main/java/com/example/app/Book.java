012b8cd7928a430b40964d1f6f37dedc89cf3f6b
Christian Neuls <christian.neuls@brandad-systems.de>
2023-06-09 12:55:25 +0200

    initial

---
+++ src/main/java/com/example/app/Book.java
@@ -0,0 +1,61 @@
+package com.example.app;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    private String isbn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO book (title, author, isbn) VALUES
+  ('The Great Gatsby', 'F. Scott Fitzgerald', '978-3-16-148410-0'),
+  ('To Kill a Mockingbird', 'Harper Lee', '978-3-16-148410-1'),
+  ('1984', 'George Orwell', '978-3-16-148410-2');
